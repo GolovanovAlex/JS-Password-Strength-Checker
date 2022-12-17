da19c0c89b1c012e3c5f92ccc317c00aa8b38faa
GolovanovAlex <Golovanov.a.a@yandex.ru>
2022-12-17 16:00:27 +0300

    js

--- app.js
+++ app.js
@@ -1,3 +1,25 @@
+function strength(password) {
+  let i = 0;
+  if (password.length > 6) {
+    i++;
+  }
+  if (password.length >= 10) {
+    i++;
+  }
+  if (/[A-Z]/.test(password)) {
+    i++;
+  }
+  if (/[0-9]/.test(password)) {
+    i++;
+  }
+  if (/[A-Za-z0-8]/.test(password)) {
+    i++;
+  }
+  return i;
+}
+
+let container = document.querySelector('.container');
+
 let pswrd = document.querySelector('#myPassword');
 let show = document.querySelector('.show');
 show.onclick = function () {
@@ -9,4 +31,3 @@ show.onclick = function () {
     show.classList.remove('hide');
   }
 };
-function strength(password) {}
